b979239ca218a347546e8c59c8be0ab173ff7250
IamWhatIam <50729504+Bloodykilla@users.noreply.github.com>
2020-06-25 23:41:14 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(function()  {
+    var header = $("#header"),
+            introH = $("#intro").innerHeight();
+            scrollOffset = 0;
+            checkScroll(scrollOffset);     
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+        checkScroll(scrollOffset);
+    });
+    function checkScroll (scrollOffset) {
+        if ( scrollOffset >= introH ){
+          header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    /*Smooth scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        var $this = $(this),
+            blockId = $(this).data('scroll'),
+            blockOffset = $(blockId).offset().top;
+       
+        $("nav a").removeClass("active");
+        $this.addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        },1500);
+    });
+    /*nav menu*/
+    $("#nav_toggle").on("click",function(event){
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+    });
+});
